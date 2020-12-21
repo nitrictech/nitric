ba079cbffe172ba0d4869f551b165a5c54aa29a6
Tim <timothy.j.holm@gmail.com>
2020-12-21 14:00:14 +1100

    chore: Implement stubs for the s3 plugin.

--- plugins/aws/storage/s3.go
+++ plugins/aws/storage/s3.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/nitric-dev/membrane/plugins/sdk"
+	"github.com/nitric-dev/membrane/utils"
+)
+
 type S3Plugin struct {
 	sdk.UnimplementedStoragePlugin
+	client *s3.S3
+}
+
+func (s *S3Plugin) Put(bucket string, key string, object []byte) error {
+	return fmt.Errorf("UNIMPLEMENTED")
+}
+
+// Retrieve an item from a bucket
+func (s *S3Plugin) Get(bucket string, key string) ([]byte, error) {
+	return nil, fmt.Errorf("UNIMPLEMENTED")
+}
+
+// Create new DynamoDB documents server
+// XXX: No External Args for function atm (currently the plugin loader does not pass any argument information)
+func New() (sdk.StoragePlugin, error) {
+	awsRegion := utils.GetEnv("AWS_REGION", "us-east-1")
+
+	sess, sessionError := session.NewSession(&aws.Config{
+		// FIXME: Use ENV configuration
+		Region: aws.String(awsRegion),
+	})
+
+	if sessionError != nil {
+		return nil, fmt.Errorf("Error creating new AWS session %v", sessionError)
+	}
+
+	s3Client := s3.New(sess)
+
+	return &S3Server{
+		client: s3Client,
+	}, nil
 }
