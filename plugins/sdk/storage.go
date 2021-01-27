6eddd7d097f311211231a097e5dfdbc97f7b4f80
Jye Cusch <jye.cusch@gmail.com>
2021-01-27 11:35:51 +1100

    fix: add missing method detection for StoragePlugin

--- plugins/sdk/storage.go
+++ plugins/sdk/storage.go
@@ -5,16 +5,22 @@ import "fmt"
 type StoragePlugin interface {
 	Get(bucket string, key string) ([]byte, error)
 	Put(bucket string, key string, object []byte) error
+	Delete(bucket string, key string) error
 }
 
 type UnimplementedStoragePlugin struct {
-	StoragePlugin
 }
+
+var _ StoragePlugin = (*UnimplementedStoragePlugin)(nil)
 
 func (*UnimplementedStoragePlugin) Get(bucket string, key string) ([]byte, error) {
 	return nil, fmt.Errorf("UNIMPLEMENTED")
 }
 
 func (*UnimplementedStoragePlugin) Put(bucket string, key string, object []byte) error {
+	return fmt.Errorf("UNIMPLEMENTED")
+}
+
+func (*UnimplementedStoragePlugin) Delete(bucket string, key string) error {
 	return fmt.Errorf("UNIMPLEMENTED")
 }
