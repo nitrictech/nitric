77386ed1f77e0a3b843aa3c11559b8ef83fea519
Tim Holm <tim.holm@nitric.io>
2021-03-22 12:44:42 +1100

    chore: replace references to documents in comments.

--- adapters/grpc/kv_grpc.go
+++ adapters/grpc/kv_grpc.go
@@ -10,10 +10,10 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-// GRPC Interface for registered Nitric Documents Plugins
+// GRPC Interface for registered Nitric KV Plugin
 type KeyValueServer struct {
 	pb.UnimplementedKeyValueServer
-	// TODO: Support multiple plugin registerations
+	// TODO: Support multiple plugin registrations
 	// Just need to settle on a way of addressing them on calls
 	kvPlugin sdk.KeyValueService
 }
@@ -31,8 +31,8 @@ func (s *KeyValueServer) Put(ctx context.Context, req *pb.KeyValuePutRequest) (*
 		if err := s.kvPlugin.Put(req.GetCollection(), req.GetKey(), req.GetValue().AsMap()); err == nil {
 			return &pb.KeyValuePutResponse{}, nil
 		} else {
-			// Case: Failed to create the document
-			// TODO: Translate from internal Documents Plugin Error
+			// Case: Failed to create the key
+			// TODO: Translate from internal KeyValue Plugin Error
 			return nil, err
 		}
 	} else {
@@ -49,18 +49,18 @@ func (s *KeyValueServer) Get(ctx context.Context, req *pb.KeyValueGetRequest) (*
 					Value: valStruct,
 				}, nil
 			} else {
-				// Case: Failed to create PB struct from stored document
-				// TODO: Translate from internal Documents Plugin Error
+				// Case: Failed to create PB struct from stored value
+				// TODO: Translate from internal KeyValue Plugin Error
 				return nil, err
 			}
 		} else {
-			// Case: There was an error retrieving the document
-			// TODO: Translate from internal Documents Plugin Error
+			// Case: There was an error retrieving the keyvalue
+			// TODO: Translate from internal KeyValue Plugin Error
 			return nil, err
 		}
 	} else {
-		// Case: The documents plugin was not registered
-		// TODO: Translate from internal Documents Plugin Error
+		// Case: The keyvalue plugin was not registered
+		// TODO: Translate from internal KeyValue Plugin Error
 		return nil, err
 	}
 }
@@ -70,8 +70,8 @@ func (s *KeyValueServer) Delete(ctx context.Context, req *pb.KeyValueDeleteReque
 		if err := s.kvPlugin.Delete(req.GetCollection(), req.GetKey()); err == nil {
 			return &pb.KeyValueDeleteResponse{}, nil
 		} else {
-			// Case: Failed to create the document
-			// TODO: Translate from internal Documents Plugin Error
+			// Case: Failed to create the keyvalue
+			// TODO: Translate from internal KeyValue Plugin Error
 			return nil, err
 		}
 	} else {
