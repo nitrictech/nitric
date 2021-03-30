f6fcfe6f0c4666d25eb466054fbf68fe557c3a8c
Tim Holm <tim.holm@nitric.io>
2021-03-30 16:36:48 +1100

    build: Update tools.

--- tools/tools.go
+++ tools/tools.go
@@ -3,5 +3,7 @@ package tools
 import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/onsi/ginkgo/ginkgo"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 )
