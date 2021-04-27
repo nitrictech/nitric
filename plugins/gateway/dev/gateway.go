56dd2bd6b1d168912bf70fea6b58c8ade694bb49
Tim Holm <tim.holm@nitric.io>
2021-04-27 10:51:27 +1000

    chore: Remove superfluous http server start from dev gateway.

--- plugins/gateway/dev/gateway.go
+++ plugins/gateway/dev/gateway.go
@@ -2,8 +2,6 @@
 package gateway_plugin
 
 import (
-	"fmt"
-	"net/http"
 	"strings"
 
 	"github.com/nitric-dev/membrane/handler"
@@ -69,57 +67,8 @@ func httpHandler(handler handler.TriggerHandler) func(ctx *fasthttp.RequestCtx) 
 }
 
 func (s *HttpGateway) Start(handler handler.TriggerHandler) error {
-
 	// Start the fasthttp server
-	fasthttp.ListenAndServe(s.address, httpHandler(handler))
-
-	// Setup the function handler for the default (catch all route)
-	//http.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
-	//	// Handle the HTTP response...
-	//	headers := req.Header
-
-	//	var triggerTypeString = headers.Get("x-nitric-source-type")
-
-	//	// Handle Event/Subscription Request Types
-	//	if strings.ToUpper(triggerTypeString) == triggers.TriggerType_Subscription.String() {
-	//		trigger := headers.Get("x-nitric-source")
-	//		requestId := headers.Get("x-nitric-request-id")
-	//		payload, _ := ioutil.ReadAll(req.Body)
-
-	//		err := handler.HandleEvent(&triggers.Event{
-	//			ID:      requestId,
-	//			Topic:   trigger,
-	//			Payload: payload,
-	//		})
-
-	//		if err != nil {
-	//			// TODO: Make this more informative
-	//			resp.WriteHeader(500)
-	//			resp.Write([]byte("There was an error processing the event"))
-	//		} else {
-	//			resp.WriteHeader(200)
-	//			resp.Write([]byte("Successfully Handled the Event"))
-	//		}
-
-	//		// return here...
-	//		return
-	//	}
-
-	//	httpReq := triggers.FromHttpRequest(req)
-	//	// Handle HTTP Request Types
-	//	response := handler.HandleHttpRequest(httpReq)
-	//	responsePayload, _ := ioutil.ReadAll(response.Body)
-
-	//	for key := range response.Header {
-	//		resp.Header().Add(key, response.Header.Get(key))
-	//	}
-
-	//	resp.WriteHeader(response.StatusCode)
-	//	resp.Write(responsePayload)
-	//})
-
-	// Start a HTTP Proxy server here...
-	httpError := http.ListenAndServe(fmt.Sprintf("%s", s.address), nil)
+	httpError := fasthttp.ListenAndServe(s.address, httpHandler(handler))
 
 	return httpError
 }
