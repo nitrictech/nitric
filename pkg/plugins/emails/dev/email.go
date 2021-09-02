5191bca6e6d8222d9fa1d6652c1a21a9417ffd0c
Jye Cusch <jye.cusch@nitric.io>
2021-09-02 17:06:09 +1000

    test: add tests for dev email plugin

--- pkg/plugins/emails/dev/email.go
+++ pkg/plugins/emails/dev/email.go
@@ -1,7 +1,10 @@
-package dev
+package email_service
 
 import (
 	"fmt"
+
+	"github.com/nitric-dev/membrane/pkg/plugins/errors"
+	"github.com/nitric-dev/membrane/pkg/plugins/errors/codes"
 
 	"github.com/nitric-dev/membrane/pkg/plugins/emails"
 )
@@ -10,11 +13,24 @@ type DevEmailService struct {
 	emails.UnimplementedEmailService
 }
 
+func isNilOrEmpty(strArray []string) bool {
+	return strArray == nil || len(strArray) == 0
+}
+
 func (s *DevEmailService) Send(from string, dest emails.EmailDestination, subject string, body emails.EmailBody) error {
+	newErr := errors.ErrorsWithScope(
+		"SesEmailService.Send",
+		fmt.Sprintf("from=%s", from),
+	)
+
+	// At least one destination email address is required.
+	if isNilOrEmpty(dest.To) && isNilOrEmpty(dest.Cc) && isNilOrEmpty(dest.Bcc) {
+		return newErr(codes.InvalidArgument, "no recipients specified", fmt.Errorf("one of to, cc or bcc must be specified"))
+	}
+
 	// Just print the request so basic debugging/testing can be performed.
 	// Future enhancement may allow SMTP setting or similar be used to send real emails.
-	fmt.Println(fmt.Sprintf("Send Email from: %s, destination: %v, subject: %s, body text: %s, body html: %s", from, dest, subject, body.Text, body.Html))
-	// TODO: consider mechanism to simulate errors
+	fmt.Println(fmt.Sprintf("Email sent from: %s, destination: %v, subject: %s, body text: %s, body html: %s", from, dest, subject, *body.Text, *body.Html))
 	return nil
 }
 
