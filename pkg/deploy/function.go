9835c6f3a09e1a60d7ad2dadf9c0c952a05a0eb9
Jye Cusch <jye.cusch@nitric.io>
2021-12-23 11:27:36 +1100

    feat: protect function maps with mutex

--- pkg/deploy/function.go
+++ pkg/deploy/function.go
@@ -2,6 +2,7 @@ package deploy
 
 import (
 	"fmt"
+	"sync"
 
 	pb "github.com/nitrictech/nitric/interfaces/nitric/v1"
 )
@@ -16,6 +17,7 @@ type Function struct {
 	collections   map[string]*pb.CollectionResource
 	queues        map[string]*pb.QueueResource
 	policies      []*pb.PolicyResource
+	lock		sync.RWMutex
 }
 
 func (a *Function) String() string {
@@ -33,10 +35,14 @@ func (a *Function) String() string {
 
 // AddPolicy - Adds an access policy dependency to the function
 func (a *Function) AddPolicy(p *pb.PolicyResource) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	a.policies = append(a.policies, p)
 }
 
 func (a *Function) AddApiHandler(aw *pb.ApiWorker) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	if a.apis[aw.Api] != nil {
 		a.apis[aw.Api] = make([]*pb.ApiWorker, 0)
 	}
@@ -46,6 +52,8 @@ func (a *Function) AddApiHandler(aw *pb.ApiWorker) {
 
 // AddSubscriptionHandler - registers a handler in the function that subscribes to a topic of events
 func (a *Function) AddSubscriptionHandler(sw *pb.SubscriptionWorker) error {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	// TODO: Determine if this subscription handler has a write policy to the same topic
 	if a.subscriptions[sw.Topic] != nil {
 		// return a new error
@@ -60,32 +68,42 @@ func (a *Function) AddSubscriptionHandler(sw *pb.SubscriptionWorker) error {
 
 // AddScheduleHandler - registers a handler in the function that runs on a schedule
 func (a *Function) AddScheduleHandler(sw *pb.ScheduleWorker) error {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	if a.schedules[sw.Key] != nil {
 		return fmt.Errorf("schedule %s already exists", sw.Key)
 	}
 
-	a.schedules[sw.Key] = sw
+	a.schedules[sw.GetKey()] = sw
 
 	return nil
 }
 
 // AddBucket - adds a storage bucket dependency to the function
 func (a *Function) AddBucket(name string, b *pb.BucketResource) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	a.buckets[name] = b
 }
 
 // AddTopic - adds a pub/sub topic dependency to the function
 func (a *Function) AddTopic(name string, t *pb.TopicResource) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	a.topics[name] = t
 }
 
 // AddCollection - adds a document database collection dependency to the function
 func (a *Function) AddCollection(name string, c *pb.CollectionResource) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	a.collections[name] = c
 }
 
 // AddQueue - adds a queue dependency to the function
 func (a *Function) AddQueue(name string, q *pb.QueueResource) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
 	a.queues[name] = q
 }
 
