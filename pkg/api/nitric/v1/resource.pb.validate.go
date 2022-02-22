3802e3d6aba01a2164b29531b85135b62ddfa593
Tim Holm <tim.holm@nitric.io>
2022-02-22 16:05:54 +1100

    feat: Add secret resource definition and actions.

--- pkg/api/nitric/v1/resource.pb.validate.go
+++ pkg/api/nitric/v1/resource.pb.validate.go
@@ -514,6 +514,37 @@ func (m *ResourceDeclareRequest) validate(all bool) error {
 			}
 		}
 
+	case *ResourceDeclareRequest_Secret:
+
+		if all {
+			switch v := interface{}(m.GetSecret()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ResourceDeclareRequestValidationError{
+						field:  "Secret",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ResourceDeclareRequestValidationError{
+						field:  "Secret",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetSecret()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ResourceDeclareRequestValidationError{
+					field:  "Secret",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	}
 
 	if len(errors) > 0 {
@@ -997,6 +1028,106 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = CollectionResourceValidationError{}
+
+// Validate checks the field values on SecretResource with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *SecretResource) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on SecretResource with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in SecretResourceMultiError,
+// or nil if none found.
+func (m *SecretResource) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *SecretResource) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return SecretResourceMultiError(errors)
+	}
+
+	return nil
+}
+
+// SecretResourceMultiError is an error wrapping multiple validation errors
+// returned by SecretResource.ValidateAll() if the designated constraints
+// aren't met.
+type SecretResourceMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SecretResourceMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SecretResourceMultiError) AllErrors() []error { return m }
+
+// SecretResourceValidationError is the validation error returned by
+// SecretResource.Validate if the designated constraints aren't met.
+type SecretResourceValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SecretResourceValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SecretResourceValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SecretResourceValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SecretResourceValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SecretResourceValidationError) ErrorName() string { return "SecretResourceValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SecretResourceValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSecretResource.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SecretResourceValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SecretResourceValidationError{}
 
 // Validate checks the field values on ResourceDeclareResponse with the rules
 // defined in the proto definition for this message. If any rules are
