5989a0268b4954aba46aa3821ba7bbb9e6997517
Tim Holm <tim.holm@nitric.io>
2022-10-13 11:00:28 +1100

    chore: reduce minimum delay.

--- pkg/api/nitric/v1/event.pb.validate.go
+++ pkg/api/nitric/v1/event.pb.validate.go
@@ -121,10 +121,10 @@ func (m *EventPublishRequest) validate(all bool) error {
 
 	if m.GetDelay() != 0 {
 
-		if val := m.GetDelay(); val < 30 || val > 2592000 {
+		if val := m.GetDelay(); val < 10 || val > 2592000 {
 			err := EventPublishRequestValidationError{
 				field:  "Delay",
-				reason: "value must be inside range [30, 2592000]",
+				reason: "value must be inside range [10, 2592000]",
 			}
 			if !all {
 				return err
