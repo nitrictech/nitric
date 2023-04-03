d87fc4a37ec06b5e1cb064194ba29ba867d56cd5
Tim Holm <tim.holm@nitric.io>
2023-04-03 15:40:41 +1000

    update GCP config to use new rules
    Co-Authored-By: Jye Cusch<jye.cusch@nitric.io>

--- cloud/gcp/deploy/config/config.go
+++ cloud/gcp/deploy/config/config.go
@@ -17,13 +17,23 @@
 package config
 
 import (
+	"fmt"
+
+	"github.com/imdario/mergo"
+	"github.com/mitchellh/mapstructure"
 	"github.com/nitrictech/nitric/cloud/common/deploy/config"
 )
 
-type GcpConfigItem struct {
-	CloudRun  GcpCloudRunConfig
+type RawConfig = config.AbstractConfig[*RawConfigItem]
+
+type RawConfigItem struct {
+	Extras    map[string]any `mapstructure:",remain"`
 	Telemetry int
-	Target    string
+}
+
+type GcpConfigItem struct {
+	CloudRun  *GcpCloudRunConfig `mapstructure:",omitempty"`
+	Telemetry int
 }
 
 type GcpCloudRunConfig struct {
@@ -34,22 +44,72 @@ type GcpCloudRunConfig struct {
 	Concurrency  int
 }
 
-type GcpConfig = config.AbstractConfig[GcpConfigItem]
+type GcpConfig = config.AbstractConfig[*GcpConfigItem]
+
+var defaultCloudRunConfig = &GcpCloudRunConfig{
+	Memory:       512,
+	Timeout:      300,
+	MinInstances: 0,
+	MaxInstances: 80,
+	Concurrency:  300,
+}
 
 var defaultGcpConfigItem = GcpConfigItem{
-	CloudRun: GcpCloudRunConfig{
-		Memory:       512,
-		Timeout:      300,
-		MinInstances: 0,
-		MaxInstances: 80,
-		Concurrency:  300,
-	},
 	Telemetry: 0,
-	Target:    "cloudrun",
 }
 
 // Return GcpConfig from stack attributes
 func ConfigFromAttributes(attributes map[string]interface{}) (*GcpConfig, error) {
-	// Use common ConfigFromAttributes
-	return config.ConfigFromAttributes(attributes, defaultGcpConfigItem)
+	rawConfig := RawConfig{}
+	err := mapstructure.Decode(attributes, &rawConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	for configName, configVal := range rawConfig.Config {
+		if configVal == nil {
+			return nil, fmt.Errorf("configuration key %s should not be empty", configName)
+		}
+
+		if len(configVal.Extras) > 1 {
+			return nil, fmt.Errorf("config items should not contain more than one runtime config")
+		}
+	}
+
+	gcpConfig := &GcpConfig{}
+	err = mapstructure.Decode(attributes, gcpConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	if gcpConfig.Config == nil {
+		gcpConfig.Config = map[string]*GcpConfigItem{}
+	}
+
+	// if no default then set provider level defaults
+	if _, hasDefault := gcpConfig.Config["default"]; !hasDefault {
+		gcpConfig.Config["default"] = &defaultGcpConfigItem
+		gcpConfig.Config["default"].CloudRun = defaultCloudRunConfig
+	}
+
+	for configName, configVal := range gcpConfig.Config {
+		// Add omitted values from default configs where needed.
+		err := mergo.Merge(configVal, defaultGcpConfigItem)
+		if err != nil {
+			return nil, err
+		}
+
+		if configVal.CloudRun == nil { // check if no runtime config provided, default to Lambda.
+			configVal.CloudRun = defaultCloudRunConfig
+		} else {
+			err := mergo.Merge(configVal.CloudRun, defaultCloudRunConfig)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		gcpConfig.Config[configName] = configVal
+	}
+
+	return gcpConfig, nil
 }
