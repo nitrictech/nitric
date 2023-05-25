3ce79e710433056a9dc7f2dcaf3bc159d5086569
Tim Holm <tim.holm@nitric.io>
2023-05-25 16:19:53 +1000

    remove legacy common tags.

--- cloud/common/deploy/tags/tags.go
+++ cloud/common/deploy/tags/tags.go
@@ -8,11 +8,6 @@ import (
 
 func Tags(ctx *pulumi.Context, stackID string, name string) map[string]string {
 	return map[string]string{
-		"x-nitric-project":    ctx.Project(),
-		"x-nitric-stack":      stackID,
-		"x-nitric-stack-name": ctx.Stack(),
-		"x-nitric-name":       name,
-		// New tag for resource identification
 		// Locate the unique stack by the presence of the key and the resource by its name
 		fmt.Sprintf("x-nitric-stack-%s", stackID): name,
 	}
