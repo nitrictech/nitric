c60bd386f50187d593ef7d97a07a6037f44bdf6a
Tim Holm <tim.holm@nitric.io>
2023-07-31 16:55:25 +1000

    aws: Implement File Exists

--- cloud/aws/ifaces/s3iface/s3.go
+++ cloud/aws/ifaces/s3iface/s3.go
@@ -22,10 +22,11 @@ import (
 )
 
 type S3API interface {
+	s3.HeadObjectAPIClient
+	s3.ListObjectsV2APIClient
 	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
 	DeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error)
-	ListObjectsV2(ctx context.Context, params *s3.ListObjectsV2Input, optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error)
 }
 
 type PreSignAPI interface {
