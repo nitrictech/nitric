9ec4ab8f766663800ffdf0aa31f9b404612b730f
Jye Cusch <jye.cusch@nitric.io>
2024-01-11 15:21:49 +1100

    add rates support (#551)

--- cloud/gcp/deploy/schedule/cloudscheduler.go
+++ cloud/gcp/deploy/schedule/cloudscheduler.go
@@ -19,10 +19,12 @@ package schedule
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/nitrictech/nitric/cloud/gcp/deploy/exec"
-	deploy "github.com/nitrictech/nitric/core/pkg/proto/deployments/v1"
+	deploymentspb "github.com/nitrictech/nitric/core/pkg/proto/deployments/v1"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/cloudscheduler"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -37,7 +39,7 @@ type CloudScheduler struct {
 type CloudSchedulerArgs struct {
 	Location string
 	Exec     *exec.CloudRunner
-	Schedule *deploy.Schedule
+	Schedule *deploymentspb.Schedule
 	Tz       string
 }
 
@@ -54,6 +56,34 @@ func NewCloudSchedulerJob(ctx *pulumi.Context, name string, args *CloudScheduler
 	err := ctx.RegisterComponentResource("nitric:schedule:GCPCloudSchedulerSchedule", name, res, opts...)
 	if err != nil {
 		return nil, err
+	}
+
+	cronExpression := ""
+
+	switch t := args.Schedule.Cadence.(type) {
+	case *deploymentspb.Schedule_Cron:
+		cronExpression = t.Cron.Expression
+	case *deploymentspb.Schedule_Every:
+		parts := strings.Split(strings.TrimSpace(t.Every.Rate), " ")
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid schedule rate: %s", t.Every.Rate)
+		}
+
+		initialRate, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return nil, fmt.Errorf("invalid schedule rate, must start with an integer")
+		}
+
+		// Google App Engine cron syntax only support hours, minutes and seconds. Convert days to hours
+		if strings.HasPrefix(parts[1], "day") {
+			// TODO: verify that rates exceeding 24 hours are supported.
+			parts[0] = fmt.Sprintf("%d", initialRate*24)
+			parts[1] = "hours"
+		}
+
+		cronExpression = fmt.Sprintf("every %s %s", parts[0], parts[1])
+	default:
+		return nil, fmt.Errorf("unknown schedule type, must be one of: cron, every")
 	}
 
 	eventJSON, err := json.Marshal(map[string]interface{}{
@@ -74,7 +104,7 @@ func NewCloudSchedulerJob(ctx *pulumi.Context, name string, args *CloudScheduler
 			},
 			Body: pulumi.String(payload),
 		},
-		Schedule: pulumi.String(strings.ReplaceAll(args.Schedule.Cron, "'", "")),
+		Schedule: pulumi.String(cronExpression),
 	}, append(opts, pulumi.Parent(res))...)
 	if err != nil {
 		return nil, err
