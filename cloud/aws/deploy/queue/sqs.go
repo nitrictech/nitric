b9dabcf4269cdbfa6e19993d4841b507896daee6
Tim Holm <tim.holm@nitric.io>
2024-02-05 12:30:15 +1100

    wip queue restoration.

---
+++ cloud/aws/deploy/queue/sqs.go
@@ -0,0 +1,56 @@
+// Copyright Nitric Pty Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package queue
+
+import (
+	"github.com/nitrictech/nitric/cloud/common/deploy/resources"
+	common "github.com/nitrictech/nitric/cloud/common/deploy/tags"
+	v1 "github.com/nitrictech/nitric/core/pkg/api/nitric/deploy/v1"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/sqs"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type SQSQueue struct {
+	pulumi.ResourceState
+	Sqs  *sqs.Queue
+	Name string
+}
+
+type SQSQueueArgs struct {
+	Queue   *v1.Queue
+	StackID string
+}
+
+func NewSQSQueue(ctx *pulumi.Context, name string, args *SQSQueueArgs, opts ...pulumi.ResourceOption) (*SQSQueue, error) {
+	res := &SQSQueue{Name: name}
+
+	err := ctx.RegisterComponentResource("nitric:queue:AwsSqsQueue", name, res, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	queue, err := sqs.NewQueue(ctx, name, &sqs.QueueArgs{
+		Tags: pulumi.ToStringMap(common.Tags(args.StackID, name, resources.Queue)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	res.Sqs = queue
+
+	return res, nil
+}
---
+++ cloud/gcp/deploy/queue.go
@@ -0,0 +1,101 @@
+// Copyright Nitric Pty Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deploy
+
+import (
+	"fmt"
+
+	"github.com/nitrictech/nitric/cloud/common/deploy/resources"
+
+	common "github.com/nitrictech/nitric/cloud/common/deploy/tags"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/pubsub"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+// type PubSubTopic struct {
+// 	pulumi.ResourceState
+
+// 	Name         string
+// 	PubSub       *pubsub.Topic
+// 	Subscription *pubsub.Subscription
+// }
+
+// type PubSubTopicArgs struct {
+// 	Location string
+// 	StackID  string
+// 	Queue    *v1.Queue
+// }
+
+func (p *NitricGcpPulumiProvider) Queue(ctx *pulumi.Context, parent pulumi.Resource, name string, config *deploymentspb.Queue) error {
+	var err error
+	opts := append([]pulumi.ResourceOption{}, pulumi.Parent(parent))
+
+	resourceLabels := common.Tags(p.stackID, name, resources.Queue)
+
+	p.queues[name], err = pubsub.NewTopic(ctx, fmt.Sprintf("%s-queue", name), &pubsub.TopicArgs{
+		Labels: pulumi.ToStringMap(resourceLabels),
+	}, opts...)
+	if err != nil {
+		return err
+	}
+
+	_, err = pubsub.NewSubscription(ctx, fmt.Sprintf("%s-nitricqueue", name), &pubsub.SubscriptionArgs{
+		Topic:  p.queues[name].Name,
+		Labels: pulumi.ToStringMap(resourceLabels),
+		ExpirationPolicy: &pubsub.SubscriptionExpirationPolicyArgs{
+			Ttl: pulumi.String(""),
+		},
+	}, opts...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewPubSubTopic(ctx *pulumi.Context, name string, args *PubSubTopicArgs, opts ...pulumi.ResourceOption) (*PubSubTopic, error) {
+	res := &PubSubTopic{
+		Name: name,
+	}
+
+	err := ctx.RegisterComponentResource("nitric:queue:GCPPubSubTopic", name, res, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	resourceLabels := common.Tags(args.StackID, name, resources.Queue)
+
+	res.PubSub, err = pubsub.NewTopic(ctx, name, &pubsub.TopicArgs{
+		Labels: pulumi.ToStringMap(resourceLabels),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	res.Subscription, err = pubsub.NewSubscription(ctx, fmt.Sprintf("%s-nitricqueue", name), &pubsub.SubscriptionArgs{
+		Topic:  res.PubSub.Name,
+		Labels: pulumi.ToStringMap(resourceLabels),
+		ExpirationPolicy: &pubsub.SubscriptionExpirationPolicyArgs{
+			Ttl: pulumi.String(""),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
