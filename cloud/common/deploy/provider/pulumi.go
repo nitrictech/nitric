d64ec0129cd9673a6fd8d864b4bc7c89bf799387
Tim Holm <tim.holm@nitric.io>
2024-02-15 07:53:30 +1100

    allow multiple deployment result types. (#581)
    * allow multiple deployment result types.
    
    * change simple -> text.

--- cloud/common/deploy/provider/pulumi.go
+++ cloud/common/deploy/provider/pulumi.go
@@ -228,7 +228,9 @@ func (s *PulumiProviderServer) Up(req *deploymentspb.DeploymentUpRequest, stream
 	stream.Send(&deploymentspb.DeploymentUpEvent{
 		Content: &deploymentspb.DeploymentUpEvent_Result{
 			Result: &deploymentspb.UpResult{
-				Details: resultStr,
+				Content: &deploymentspb.UpResult_Text{
+					Text: resultStr,
+				},
 			},
 		},
 	})
