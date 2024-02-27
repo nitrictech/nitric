97e4704be817a61eea4ce9d1b5a7297dd0be483d
Jye Cusch <jye.cusch@nitric.io>
2024-02-27 19:51:20 +1100

    fix(azure): fix deployments for stacks with kv stores and no buckets

--- cloud/azure/deploy/deploy.go
+++ cloud/azure/deploy/deploy.go
@@ -244,11 +244,11 @@ func (a *NitricAzurePulumiProvider) Pre(ctx *pulumi.Context, nitricResources []*
 		}
 	}
 
-	// Create a storage account if buckets
+	// Create a storage account if buckets or key/value stores are required.
 	// Unlike AWS and GCP which have centralized storage management, Azure allows for multiple storage accounts.
 	// This means we need to create a storage account for each stack, before buckets can be created.
-	if hasResourceType(nitricResources, resourcespb.ResourceType_Bucket) {
-		logger.Info("Stack declares one or more buckets, creating stack level Azure Storage Account")
+	if hasResourceType(nitricResources, resourcespb.ResourceType_Bucket) || hasResourceType(nitricResources, resourcespb.ResourceType_KeyValueStore) {
+		logger.Info("Stack declares bucket(s) or key/value store(s), creating stack level Azure Storage Account")
 		a.storageAccount, err = createStorageAccount(ctx, a.resourceGroup, tags.Tags(a.stackId, ctx.Stack(), commonresources.Stack))
 		if err != nil {
 			return errors.WithMessage(err, "storage account create")
