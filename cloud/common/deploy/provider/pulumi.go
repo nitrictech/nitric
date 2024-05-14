2992a88ffc926f71df92b787866f7372a2563ba9
Tim Holm <tim.holm@nitric.io>
2024-05-14 13:44:10 +1000

    remove preview debugging.

--- cloud/common/deploy/provider/pulumi.go
+++ cloud/common/deploy/provider/pulumi.go
@@ -243,42 +243,30 @@ func (s *PulumiProviderServer) Up(req *deploymentspb.DeploymentUpRequest, stream
 		}
 	}
 
-	var resultStr = ""
-
-	preview, ok := attributesMap["preview"].(bool)
-	if ok && preview {
-		result, err := autoStack.Preview(context.TODO())
-
-		if err != nil {
-			return err
-		}
-		resultStr = result.StdOut
-	} else {
-		result, err := autoStack.Up(context.TODO(), optup.EventStreams(pulumiEventsChan))
-		if err != nil {
-			// Check for common Pulumi 'autoError' types
-			if auto.IsConcurrentUpdateError(err) {
-				if pe := parsePulumiError(err); pe != nil {
-					err = pe
-				}
-				err = fmt.Errorf("the pulumi stack file is locked.\nThis occurs when a previous deployment is still in progress or was interrupted.\n%w", err)
-			} else if auto.IsSelectStack404Error(err) {
-				err = fmt.Errorf("stack not found. %w", err)
-			} else if auto.IsCreateStack409Error(err) {
-				err = fmt.Errorf("failed to create Pulumi stack, this may be a bug in nitric. Seek help https://github.com/nitrictech/nitric/issues\n%w", err)
-			} else if auto.IsCompilationError(err) {
-				err = fmt.Errorf("failed to compile Pulumi program, this may be a bug in your chosen provider or with nitric. Seek help https://github.com/nitrictech/nitric/issues\n%w", err)
-			} else if pe := parsePulumiError(err); pe != nil {
+	result, err := autoStack.Up(context.TODO(), optup.EventStreams(pulumiEventsChan))
+	if err != nil {
+		// Check for common Pulumi 'autoError' types
+		if auto.IsConcurrentUpdateError(err) {
+			if pe := parsePulumiError(err); pe != nil {
 				err = pe
 			}
-
-			return err
+			err = fmt.Errorf("the pulumi stack file is locked.\nThis occurs when a previous deployment is still in progress or was interrupted.\n%w", err)
+		} else if auto.IsSelectStack404Error(err) {
+			err = fmt.Errorf("stack not found. %w", err)
+		} else if auto.IsCreateStack409Error(err) {
+			err = fmt.Errorf("failed to create Pulumi stack, this may be a bug in nitric. Seek help https://github.com/nitrictech/nitric/issues\n%w", err)
+		} else if auto.IsCompilationError(err) {
+			err = fmt.Errorf("failed to compile Pulumi program, this may be a bug in your chosen provider or with nitric. Seek help https://github.com/nitrictech/nitric/issues\n%w", err)
+		} else if pe := parsePulumiError(err); pe != nil {
+			err = pe
 		}
 
-		resultStr, ok = result.Outputs[resultCtxKey].Value.(string)
-		if !ok {
-			resultStr = ""
-		}
+		return err
+	}
+
+	resultStr, ok := result.Outputs[resultCtxKey].Value.(string)
+	if !ok {
+		resultStr = ""
 	}
 
 	err = stream.Send(&deploymentspb.DeploymentUpEvent{
