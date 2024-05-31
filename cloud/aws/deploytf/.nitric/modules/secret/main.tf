ae97bf034655c7c0ecabdc725636b0d7906b3887
Jye Cusch <jye.cusch@nitric.io>
2024-05-31 15:09:30 +1000

    fix variable name typo

--- cloud/aws/deploytf/.nitric/modules/secret/main.tf
+++ cloud/aws/deploytf/.nitric/modules/secret/main.tf
@@ -3,7 +3,7 @@
 resource "aws_secretsmanager_secret" "secret" {
   name = var.secret_name
   tags = {
-    "x-nitric-${var.var.stack_id}-name" = var.secret_name
-    "x-nitric-${var.var.stack_id}-type" = "secret"
+    "x-nitric-${var.stack_id}-name" = var.secret_name
+    "x-nitric-${var.stack_id}-type" = "secret"
   }
 }
