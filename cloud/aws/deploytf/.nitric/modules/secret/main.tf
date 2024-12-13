aa3262012108ceb2b6f1b20ccd139311be1f8db9
Tim Holm <tim.holm@nitric.io>
2024-12-13 11:59:46 +1100

    fix(awstf): add random id for secrets

--- cloud/aws/deploytf/.nitric/modules/secret/main.tf
+++ cloud/aws/deploytf/.nitric/modules/secret/main.tf
@@ -1,7 +1,18 @@
 
+
+resource "random_id" "secret_id" {
+  byte_length = 4
+
+  prefix = "${var.secret_name}-"
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    secret_name = var.secret_name
+  }
+}
+
 # Create a new AWS secret manager secret
 resource "aws_secretsmanager_secret" "secret" {
-  name = var.secret_name
+  name = random_id.secret_id.hex
   tags = {
     "x-nitric-${var.stack_id}-name" = var.secret_name
     "x-nitric-${var.stack_id}-type" = "secret"
