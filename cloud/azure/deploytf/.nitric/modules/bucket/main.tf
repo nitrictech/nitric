8e3255cbb0c55100749311232bb62b1f710687d6
Jye Cusch <jye.cusch@nitric.io>
2025-02-18 13:30:00 +1100

    fix bucket name
    update to match expected runtime convention. To be improved using runtime resolver

--- cloud/azure/deploytf/.nitric/modules/bucket/main.tf
+++ cloud/azure/deploytf/.nitric/modules/bucket/main.tf
@@ -10,7 +10,9 @@ resource "random_id" "storage_id" {
 
 # Create a new azure blob container
 resource "azurerm_storage_container" "container" {
-  name                  = "${var.name}st${random_id.storage_id.hex}"
+  # TODO: implement random suffix (requires runtime lookup)
+  # name                  = "${var.name}st${random_id.storage_id.hex}"
+  name                  = var.name
   storage_account_id    = var.storage_account_id
   container_access_type = "private"
 }
@@ -29,9 +31,9 @@ resource "azurerm_eventgrid_event_subscription" "subscription" {
     event_time_to_live    = 5
   }
   webhook_endpoint {
-    max_events_per_batch = 1
+    max_events_per_batch           = 1
     active_directory_app_id_or_uri = each.value.active_directory_app_id_or_uri
-    active_directory_tenant_id = each.value.active_directory_tenant_id
-    url = "${each.value.url}/${each.value.event_token}/x-nitric-notification/bucket/${var.name}"
+    active_directory_tenant_id     = each.value.active_directory_tenant_id
+    url                            = "${each.value.url}/${each.value.event_token}/x-nitric-notification/bucket/${var.name}"
   }
 }
