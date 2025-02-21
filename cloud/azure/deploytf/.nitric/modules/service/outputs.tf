56995f2604a5c87e820801dd89f7987b30d5068d
Jye Cusch <jye.cusch@nitric.io>
2025-02-21 12:20:47 +1100

    use persistent endpoint instead of latest revision
    also fixes a race condition deploying revisions and updating the API targets

--- cloud/azure/deploytf/.nitric/modules/service/outputs.tf
+++ cloud/azure/deploytf/.nitric/modules/service/outputs.tf
@@ -1,45 +1,47 @@
 output "container_app_id" {
-    description = "The id of the container app"
-    value       = azurerm_container_app.container_app.id
+  description = "The id of the container app"
+  value       = azurerm_container_app.container_app.id
 }
 
 output "dapr_app_id" {
-    description = "The id of the dapr app"
-    value       = local.container_app_name
+  description = "The id of the dapr app"
+  value       = local.container_app_name
 }
 
 output "fqdn" {
-    description = "The endpoint of the container app"
-    value       = azurerm_container_app.container_app.latest_revision_fqdn
+  description = "The endpoint of the container app"
+  value       = azurerm_container_app.container_app.latest_revision_fqdn
 }
 
 output "event_token" {
-    description = "The event token of the container app"
-    value       = random_string.event_token.result
+  description = "The event token of the container app"
+  value       = random_string.event_token.result
 }
 
 output "client_id" {
-    description = "The client id of the container app"
-    value       = azuread_service_principal.service_identity.client_id
+  description = "The client id of the container app"
+  value       = azuread_service_principal.service_identity.client_id
 }
 
 output "client_secret" {
-    description = "The client secret of the container app"
-    value       = azuread_service_principal_password.service_identity.value
+  description = "The client secret of the container app"
+  value       = azuread_service_principal_password.service_identity.value
 }
 output "service_principal_id" {
-    description = "The service principal id of the container app"
-    value       = azuread_service_principal.service_identity.object_id
+  description = "The service principal id of the container app"
+  value       = azuread_service_principal.service_identity.object_id
 }
 
 output "tenant_id" {
-    description = "The tenant id of the container app"
-    value       = data.azurerm_client_config.current.tenant_id
+  description = "The tenant id of the container app"
+  value       = data.azurerm_client_config.current.tenant_id
 }
 
 output "endpoint" {
-    description = "The endpoint of the container app"
-    value       = "https://${azurerm_container_app.container_app.latest_revision_fqdn}"
+  description = "The endpoint of the container app"
+  # Gets the persistent endpoint of the container app, not the latest revision
+  # See: https://github.com/hashicorp/terraform-provider-azurerm/issues/26559
+  value = "https://${azurerm_container_app.container_app.ingress[0].fqdn}"
 }
 
 output "poll_url" {
