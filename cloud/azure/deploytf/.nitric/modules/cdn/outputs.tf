e07caed9178436964aef5e6edd60747752ff1531
David Moore <davemooreuws@gmail.com>
2025-03-12 17:27:02 +1100

    add todos

--- cloud/azure/deploytf/.nitric/modules/cdn/outputs.tf
+++ cloud/azure/deploytf/.nitric/modules/cdn/outputs.tf
@@ -1,4 +1,5 @@
-output "cdn_url" {
-  description = "The URL of the CDN endpoint"
-  value = "https://${jsondecode(azapi_resource.cdn_endpoint.output).properties.hostName}"
-}
+# TODO - add outputs for the CDN endpoint
+# output "cdn_url" {
+#   description = "The URL of the CDN endpoint"
+#   value = "https://${jsondecode(azapi_resource.cdn_endpoint.output).properties.hostName}"
+# }
