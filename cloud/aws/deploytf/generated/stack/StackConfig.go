9c0170edc32fb9110c16e4cdcaaf8c7301b13e76
Ryan Cartwright <fryzee.cartwright@gmail.com>
2025-03-14 16:39:39 +1100

    add generated code

--- cloud/aws/deploytf/generated/stack/StackConfig.go
+++ cloud/aws/deploytf/generated/stack/StackConfig.go
@@ -13,5 +13,11 @@ type StackConfig struct {
 	Providers *[]interface{} `field:"optional" json:"providers" yaml:"providers"`
 	// Experimental.
 	SkipAssetCreationFromLocalModules *bool `field:"optional" json:"skipAssetCreationFromLocalModules" yaml:"skipAssetCreationFromLocalModules"`
+	// Enable the creation of a website.
+	EnableWebsite *bool `field:"optional" json:"enableWebsite" yaml:"enableWebsite"`
+	// The root error document for the website 404.html.
+	WebsiteRootErrorDocument *string `field:"optional" json:"websiteRootErrorDocument" yaml:"websiteRootErrorDocument"`
+	// The root index document for the website index.html.
+	WebsiteRootIndexDocument *string `field:"optional" json:"websiteRootIndexDocument" yaml:"websiteRootIndexDocument"`
 }
 
