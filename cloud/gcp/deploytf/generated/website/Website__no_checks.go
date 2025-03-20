281db77ef09bedb8c7eecd7be0481f1553006d68
Tim Holm <tim.holm@nitric.io>
2025-03-21 09:46:56 +1100

    Add empty website module

---
+++ cloud/gcp/deploytf/generated/website/Website__no_checks.go
@@ -0,0 +1,38 @@
+//go:build no_runtime_type_checking
+
+package website
+
+// Building without runtime type checking enabled, so all the below just return nil
+
+func (w *jsiiProxy_Website) validateAddOverrideParameters(path *string, value interface{}) error {
+	return nil
+}
+
+func (w *jsiiProxy_Website) validateAddProviderParameters(provider interface{}) error {
+	return nil
+}
+
+func (w *jsiiProxy_Website) validateGetStringParameters(output *string) error {
+	return nil
+}
+
+func (w *jsiiProxy_Website) validateInterpolationForOutputParameters(moduleOutput *string) error {
+	return nil
+}
+
+func (w *jsiiProxy_Website) validateOverrideLogicalIdParameters(newLogicalId *string) error {
+	return nil
+}
+
+func validateWebsite_IsConstructParameters(x interface{}) error {
+	return nil
+}
+
+func validateWebsite_IsTerraformElementParameters(x interface{}) error {
+	return nil
+}
+
+func validateNewWebsiteParameters(scope constructs.Construct, id *string, config *WebsiteConfig) error {
+	return nil
+}
+
