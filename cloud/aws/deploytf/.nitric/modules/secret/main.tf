172bbd29dd7959d81d08e4f39f6725515ab4ee1c
Tim Holm <timothy.j.holm@gmail.com>
2025-05-01 12:54:11 +1000

    Allow awstf to import existing secrets.

--- cloud/aws/deploytf/.nitric/modules/secret/main.tf
+++ cloud/aws/deploytf/.nitric/modules/secret/main.tf
@@ -1,6 +1,9 @@
 
 # Create a new AWS secret manager secret
 resource "aws_secretsmanager_secret" "secret" {
+  # Only create a new secret if we're not reusing an existing one
+  count = var.existing_secret_arn == "" ? 1 : 0
+
   name = var.secret_name
   tags = {
     "x-nitric-${var.stack_id}-name" = var.secret_name
--- cloud/aws/deploytf/.nitric/modules/secret/outputs.tf
+++ cloud/aws/deploytf/.nitric/modules/secret/outputs.tf
@@ -1,4 +1,4 @@
 output "secret_arn" {
   description = "The ARN of the secret"
-  value       =  aws_secretsmanager_secret.secret.arn
+  value       = var.existing_secret_arn == "" ? one(aws_secretsmanager_secret.secret).arn : var.existing_secret_arn
 }
