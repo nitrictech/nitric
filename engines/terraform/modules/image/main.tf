b407bedc2cd7766850a68a1177f862509340be6f
Tim Holm <timothy.j.holm@gmail.com>
2025-05-28 13:20:20 +1000

    update note.

--- engines/terraform/modules/image/main.tf
+++ engines/terraform/modules/image/main.tf
@@ -4,8 +4,8 @@ resource "docker_image" "base_service" {
   dynamic "build" {
     for_each = var.image_id == null ? [1] : []
     content {
-      # TODO: 
       builder = "default"
+      # NOTE: This assumes the terraform output is three dirs down from the root of the project
       context = "${path.root}/../../../${var.build_context != "." ? var.build_context : ""}"
       dockerfile = "${path.root}/../../../${var.dockerfile}"
       tag     = ["${var.tag}:base"]
