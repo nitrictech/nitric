283e284a0c8fc455122db1a6a3860016cd12c2fe
Tim Holm <timothy.j.holm@gmail.com>
2025-06-05 09:56:40 +1000

    fix image module reference

--- engines/terraform/image.go
+++ engines/terraform/image.go
@@ -1,3 +1,3 @@
 package terraform
 
-var imageModuleRef = "github.com/jyecusch/nitric//engines/terraform/modules/image?depth=1&ref=platform-files"
+var imageModuleRef = "github.com/nitrictech/nitric//engines/terraform/modules/image?depth=1&ref=next"
