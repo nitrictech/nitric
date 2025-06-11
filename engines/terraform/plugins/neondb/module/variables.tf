bf443fa1c676b5bab39634981b1ceca9fa6d8dcf
Tim Holm <timothy.j.holm@gmail.com>
2025-06-11 13:36:21 +1000

    allow branch id to be optional for neon

--- engines/terraform/plugins/neondb/module/variables.tf
+++ engines/terraform/plugins/neondb/module/variables.tf
@@ -15,7 +15,7 @@ variable "nitric" {
 variable "existing" {
     type = object({
         project_id = string
-        branch_id = string
+        branch_id = optional(string, null)
         role_name = optional(string, null)
         database_name = optional(string, true)
         endpoint_id = optional(string, null)
