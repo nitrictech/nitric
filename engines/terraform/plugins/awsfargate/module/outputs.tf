e94f551e39e62e21f55ed0a021aa4d02ffd246e2
Tim Holm <timothy.j.holm@gmail.com>
2025-06-12 14:25:39 +1000

    update fargate module domain name.

--- engines/terraform/plugins/awsfargate/module/outputs.tf
+++ engines/terraform/plugins/awsfargate/module/outputs.tf
@@ -1,7 +1,7 @@
 output "nitric" {
   value = {
     id          = aws_lambda_function.function.arn
-    domain_name = ""
+    domain_name = data.aws_lb.alb.dns_name
     exports = {
       resources = {
         "aws_lb" = var.alb_arn
