e6ddd30ef5a45362c1a1a5d9d71580b905f3c77f
Tim Holm <timothy.j.holm@gmail.com>
2025-06-12 14:57:15 +1000

    update vpc outputs

--- engines/terraform/plugins/awsvpc/module/outputs.tf
+++ engines/terraform/plugins/awsvpc/module/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
     description = "ID of the created VPC"
 }
 
-output "private_subnet" {
+output "private_subnets" {
     value = module.vpc.private_subnets
     description = "Private subnet IDs"
 }
@@ -11,4 +11,14 @@ output "private_subnet" {
 output "public_subnets" {
     value = module.vpc.public_subnets
     description = "Public subnet IDs"
+}
+
+output "subnets" {
+    value = concat(module.vpc.private_subnets, module.vpc.public_subnets)
+    description = "All subnet IDs"
+}
+
+output "default_security_group_id" {
+    value = module.vpc.default_security_group_id
+    description = "Default security group ID"
 }
