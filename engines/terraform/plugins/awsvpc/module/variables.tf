a85818cc02fcb46eaf2eaf5a2eddbf79c83c2405
Tim Holm <timothy.j.holm@gmail.com>
2025-06-12 17:09:25 +1000

    use strings.

--- engines/terraform/plugins/awsvpc/module/variables.tf
+++ engines/terraform/plugins/awsvpc/module/variables.tf
@@ -42,10 +42,10 @@ variable "default_security_group_ingress" {
 variable "default_security_group_egress" {
     type = list(map(string))
     default = [{
-        from_port = 0
-        to_port = 0
+        from_port = "0"
+        to_port = "0"
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = "0.0.0.0/0"
     }]
 }
 
