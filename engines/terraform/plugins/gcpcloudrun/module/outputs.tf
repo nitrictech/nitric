b613d60f481d280217590f0c983e481fb4b81317
Ryan Cartwright <fryzee.cartwright@gmail.com>
2025-06-13 12:18:40 +1000

    add outputs and handle plugin registration errors

--- engines/terraform/plugins/gcpcloudrun/module/outputs.tf
+++ engines/terraform/plugins/gcpcloudrun/module/outputs.tf
@@ -2,6 +2,11 @@ output "nitric" {
     value = {
         id = google_cloud_run_v2_service.service.name
         http_endpoint = google_cloud_run_v2_service.service.uri
+        exports = {
+            resources = {
+                "google_cloud_run_v2_service" = google_cloud_run_v2_service.service.name
+            }
+        }
     }
 }
 
