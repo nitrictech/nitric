2e8eda6e2a7a72c8b42be46ec5b2913cd23d5e81
Tim Holm <timothy.j.holm@gmail.com>
2025-06-13 12:28:15 +1000

    update config.

--- engines/terraform/plugins/awsfargate/module/outputs.tf
+++ engines/terraform/plugins/awsfargate/module/outputs.tf
@@ -5,6 +5,8 @@ output "nitric" {
     exports = {
       resources = {
         "aws_lb" = var.alb_arn
+        # The security group that the for this service is attached to
+        "aws_lb:security_group" = var.alb_security_group
         # The target port that this service has attached a listener for
         "aws_lb:http_port" = var.container_port
       }
