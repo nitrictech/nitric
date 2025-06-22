d5d2c839f54e989e80e304d71a86bec6962e2d57
Tim Holm <timothy.j.holm@gmail.com>
2025-06-23 08:39:35 +1000

    add base-path to entrypoint route config

--- cli/pkg/schema/entrypoint.go
+++ cli/pkg/schema/entrypoint.go
@@ -28,4 +28,5 @@ func (e EntrypointIntent) JSONSchemaExtend(schema *jsonschema.Schema) {
 
 type Route struct {
 	TargetName string `json:"name" yaml:"name"`
+	BasePath   string `json:"base-path" yaml:"base-path"`
 }
