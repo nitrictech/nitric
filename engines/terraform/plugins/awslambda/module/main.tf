3a692f889b550989f5f8ddc38ef72d729840cd72
Tim Holm <timothy.j.holm@gmail.com>
2025-06-24 09:55:23 +1000

    use eventbridge scheduler for lambda schedules

--- engines/terraform/plugins/awslambda/module/main.tf
+++ engines/terraform/plugins/awslambda/module/main.tf
@@ -113,35 +113,51 @@ resource "aws_lambda_function_url" "endpoint" {
   # }
 }
 
-# Deploy schedule events per schedule for this service
-resource "aws_cloudwatch_event_rule" "schedule" {
-  for_each = var.nitric.schedules
-
-  name                = "${var.nitric.name}-${each.key}-schedule"
-  description         = "Schedule for ${var.nitric.name} - ${each.key}"
-  schedule_expression = local.convert_cron_to_aws[each.key].aws_cron
-}
-
-resource "aws_cloudwatch_event_target" "schedule_target" {
-  for_each = var.nitric.schedules
-
-  rule      = aws_cloudwatch_event_rule.schedule[each.key].name
-  target_id = "${var.nitric.name}-${each.key}-target"
-  arn       = aws_lambda_function.function.arn
-
-  input = jsonencode({
-    # Events will be forwarded to the lambda function with the path as the event payload
-    path = each.value.path
+# Create role and policy to allow schedule to invoke lambda
+resource "aws_iam_role" "role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
   })
 }
 
-resource "aws_lambda_permission" "allow_cloudwatch" {
-  for_each = var.nitric.schedules
-
-  statement_id  = "AllowExecutionFromCloudWatch-${each.key}"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.function.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.schedule[each.key].arn
+resource "aws_iam_role_policy" "role_policy" {
+  role = aws_iam_role.role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "lambda:InvokeFunction",
+        Resource = aws_lambda_function.function.arn
+      }
+    ]
+  })
 }
 
+# Create an AWS eventbridge schedule
+resource "aws_scheduler_schedule" "schedule" {
+  for_each = var.nitric.schedules
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = local.convert_cron_to_aws[each.key].aws_cron
+
+  target {
+    arn      = aws_lambda_function.function.arn
+    role_arn = aws_iam_role.role.arn
+
+    input = jsonencode({
+        "path" = each.value.path
+    })
+  }
+}
