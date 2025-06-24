970cdf6849b8f2f08044d9d45a7d47f559e3b66b
Tim Holm <timothy.j.holm@gmail.com>
2025-06-24 13:21:03 +1000

    remove hack types

--- cli/pkg/schema/service.go
+++ cli/pkg/schema/service.go
@@ -8,11 +8,6 @@ type ServiceIntent struct {
 	Dev *Dev `json:"dev,omitempty" yaml:"dev,omitempty"`
 
 	Triggers map[string]*ServiceTrigger `json:"triggers,omitempty" yaml:"triggers,omitempty"`
-
-	// Only used for schema generation, will always be nil. Do not use or remove.
-	ServiceSchemaOnlyHackType string `json:"type" yaml:"-" jsonschema:"type,enum=service"`
-	// TODO: should sub-type be sub_type?
-	ServiceSchemaOnlyHackSubType string `json:"sub-type,omitempty" yaml:"-,omitempty" jsonschema:"sub-type"`
 }
 
 func (s *ServiceIntent) GetType() string {
