6ad9b83c3895ddef056b12ff18fba4e1320eb1f0
Ryan Cartwright <39504851+HomelessDinosaur@users.noreply.github.com>
2025-06-30 14:42:25 +1000

    Add cloud storage as possible CDN backend (#839)

--- engines/terraform/plugins/gcpstorage/module/outputs.tf
+++ engines/terraform/plugins/gcpstorage/module/outputs.tf
@@ -1,6 +1,7 @@
 output "nitric" {
     value = {
         id = google_storage_bucket.bucket.id
+        domain_name = google_storage_bucket.bucket.url
         exports = {
             resources = {
                 "google_storage_bucket" = google_storage_bucket.bucket.id
