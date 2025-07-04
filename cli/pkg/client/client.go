e2f6ecce3309716f53afe987612e0e5fafb5195d
Ryan Cartwright <fryzee.cartwright@gmail.com>
2025-07-04 15:45:35 +1000

    rebase and remove bucket client when no buckets are present in spec

--- cli/pkg/client/client.go
+++ cli/pkg/client/client.go
@@ -1,12 +1,16 @@
 package client
 
 import (
-	"fmt"
-
 	"github.com/nitrictech/nitric/cli/pkg/schema"
-	"github.com/spf13/afero"
 )
 
-func GeneratePython(fs afero.Fs, appSpec schema.Application, outputDir string) error {
-	return fmt.Errorf("python SDK generation not implemented")
+func SpecHasClientResources(appSpec schema.Application) bool {
+	for _, intent := range appSpec.GetResourceIntents() {
+		// TODO: Add other adaptable resources here.
+		if intent.GetType() == "bucket" {
+			return true
+		}
+	}
+
+	return false
 }
