b0669fd36937f4d00fd3bb9a914c01446555dedb
Jye Cusch <jye.cusch@nitric.io>
2025-07-15 13:27:42 +1000

    allow listing all config keys and descriptions (#864)

--- cli/cmd/config.go
+++ cli/cmd/config.go
@@ -2,38 +2,92 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/charmbracelet/lipgloss"
 	"github.com/nitrictech/nitric/cli/internal/config"
 	"github.com/nitrictech/nitric/cli/internal/style"
-	"github.com/nitrictech/nitric/cli/internal/style/colors"
 	"github.com/samber/do/v2"
 	"github.com/spf13/cobra"
 )
 
+func makeRelativeIfInCurrentDir(file string) string {
+	wd, err := os.Getwd()
+	if err != nil {
+		return file
+	}
+	rel, err := filepath.Rel(wd, file)
+	if err != nil {
+		return file
+	}
+	if strings.HasPrefix(rel, "..") {
+		return file
+	}
+	return rel
+}
+
 // NewConfigCmd creates the config command
 func NewConfigCmd(injector do.Injector) *cobra.Command {
+	var listFlag bool
+
 	configCmd := &cobra.Command{
 		Use:   "config",
 		Short: "Manage CLI configuration",
-		Long:  `Manage the CLI configuration.`,
+		Long: `Manage the CLI configuration.
+
+By default, this command displays the current configuration values.
+Use the --list flag to see all available configuration keys and their descriptions.`,
+		Example: `
+# Display the current configuration values
+nitric config
+
+# List all available configuration keys and their descriptions
+nitric config --list
+		`,
 		Run: func(cmd *cobra.Command, args []string) {
 			conf := do.MustInvoke[*config.Config](injector)
 
-			if conf.FileUsed() != "" {
-				fmt.Printf("file: %s\n\n", style.Teal(conf.FileUsed()))
-				fmt.Println(lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true, false, false, false).BorderForeground(colors.Teal).Render(conf.Dump()))
-			} else {
-				fmt.Println("file: none (using defaults)")
+			underline := style.Teal("=============================")
+
+			if listFlag {
+				fmt.Println("Available configuration keys:")
+				fmt.Println(underline)
+				for _, key := range conf.AllKeysWithDescriptions() {
+					fmt.Printf("%s: %s\n", key.Path, key.Description)
+				}
+				return
 			}
+
+			file := conf.FileUsed()
+			if file != "" {
+				file = makeRelativeIfInCurrentDir(file)
+			} else {
+				file = "none (using defaults)"
+			}
+
+			fmt.Printf("Config file: %s\n", style.Teal(file))
+			fmt.Println(underline)
+			fmt.Println(conf.Dump())
 		},
 	}
 
+	configCmd.Flags().BoolVarP(&listFlag, "list", "l", false, "List all available configuration keys and their descriptions")
+
+	var setGlobalFlag bool
+
 	configSetCmd := &cobra.Command{
 		Use:   "set <key> <value>",
-		Short: "Set the CLI configuration",
-		Long:  `Set the CLI configuration.`,
-		Args:  cobra.ExactArgs(2),
+		Short: "Set a configuration value.",
+		Example: `
+# Set the URL to the Nitric server
+nitric config set url https://app.nitric.io
+
+# Set the URL to the Nitric server and save to the global config file
+nitric config set url https://app.nitric.io --global
+		`,
+		Long: `Set a value in the cli configuration file.`,
+		Args: cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			key := args[0]
 			value := args[1]
@@ -44,13 +98,15 @@ func NewConfigCmd(injector do.Injector) *cobra.Command {
 				fmt.Printf("error setting config: %v", err)
 			}
 
-			if err := conf.Save(); err != nil {
+			if err := conf.Save(setGlobalFlag); err != nil {
 				fmt.Printf("error saving config: %v", err)
 			}
 
 			fmt.Printf("Config set: %s: %s\n", key, value)
 		},
 	}
+
+	configSetCmd.Flags().BoolVarP(&setGlobalFlag, "global", "g", false, "Save the configuration to the global config file")
 
 	configCmd.AddCommand(configSetCmd)
 	return configCmd
