c5fe11a305937204a48c19fd00784a1d07e1e263
Jye Cusch <jye.cusch@gmail.com>
2025-08-02 08:51:03 +1000

    log previously unhandled stream errors

--- cloud/common/deploy/provider/pulumi.go
+++ cloud/common/deploy/provider/pulumi.go
@@ -236,7 +236,10 @@ func (s *PulumiProviderServer) Up(req *deploymentspb.DeploymentUpRequest, stream
 
 	go func() {
 		// output the stream
-		_ = pulumix.StreamPulumiUpEngineEvents(stream, pulumiEventsChan)
+		err := pulumix.StreamPulumiUpEngineEvents(stream, pulumiEventsChan)
+		if err != nil {
+			logger.Errorf("error streaming Pulumi events: %v", err)
+		}
 	}()
 
 	config, err := s.provider.Config()
@@ -267,7 +270,7 @@ func (s *PulumiProviderServer) Up(req *deploymentspb.DeploymentUpRequest, stream
 		resultStr = ""
 	}
 
-	err = stream.Send(&deploymentspb.DeploymentUpEvent{
+	return stream.Send(&deploymentspb.DeploymentUpEvent{
 		Content: &deploymentspb.DeploymentUpEvent_Result{
 			Result: &deploymentspb.UpResult{
 				Content: &deploymentspb.UpResult_Text{
@@ -276,8 +279,6 @@ func (s *PulumiProviderServer) Up(req *deploymentspb.DeploymentUpRequest, stream
 			},
 		},
 	})
-
-	return err
 }
 
 // Down - automatically called by the Nitric CLI via the `down` command
@@ -308,7 +309,10 @@ func (s *PulumiProviderServer) Down(req *deploymentspb.DeploymentDownRequest, st
 	pulumiEventsChan := make(chan events.EngineEvent)
 
 	go func() {
-		_ = pulumix.StreamPulumiDownEngineEvents(stream, pulumiEventsChan)
+		err = pulumix.StreamPulumiDownEngineEvents(stream, pulumiEventsChan)
+		if err != nil {
+			logger.Errorf("error streaming Pulumi events: %v", err)
+		}
 	}()
 
 	config, err := s.provider.Config()
