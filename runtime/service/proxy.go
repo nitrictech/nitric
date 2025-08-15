fd0c20b7c79bf2948dbbf4bd8e3f7957cbe30c45
Tim Holm <timothy.j.holm@gmail.com>
2025-08-15 16:06:50 +1000

    Ensure redirects are passed through to the client and not handled.

--- runtime/service/proxy.go
+++ runtime/service/proxy.go
@@ -14,12 +14,19 @@ type HttpServerProxy struct {
 	target string
 }
 
+var defaultHttpClient = &http.Client{
+	CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		// Prevent redirects to ensure we handle them manually if needed
+		return http.ErrUseLastResponse
+	},
+}
+
 func (p *HttpServerProxy) Forward(ctx context.Context, req *http.Request) (*http.Response, error) {
 	// rewrite the request to the target
 	req.URL.Host = p.target
 	req.URL.Scheme = "http"
 
-	return http.DefaultClient.Do(req)
+	return defaultHttpClient.Do(req)
 }
 
 func (p *HttpServerProxy) Host() string {
